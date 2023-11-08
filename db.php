191d0c5e415666ff6e8476ae5f123c644fa19f90
Wenxuan Zhu <115663648+vincezhu0322@users.noreply.github.com>
2023-11-08 15:37:02 -0800

    Create db.php

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'database_host';
+$db   = 'database_name';
+$user = 'database_username';
+$pass = 'database_password';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
